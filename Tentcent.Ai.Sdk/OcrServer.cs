eedff1bbcb9b6afe13312de96c15c983bc27d335
BurningMyself <1928881525@qq.com>
2018-09-06 13:49:26 +0800

    添加接口
    Signed-off-by: BurningMyself <1928881525@qq.com>

--- Tentcent.Ai.Sdk/OcrServer.cs
+++ Tentcent.Ai.Sdk/OcrServer.cs
@@ -12,7 +12,7 @@ namespace Tentcent.Ai.Sdk
         {
             BaseData sendData = new BaseData();
             sendData.SetValue("app_id", dic["app_id"]);
-            sendData.SetValue("time_stamp", BaseData.GetTimeStamp(DateTime.Now,10));
+            sendData.SetValue("time_stamp", BaseData.GetTimeStamp(DateTime.Now, 10));
             sendData.SetValue("nonce_str", BaseData.GenerateOutTradeNo());
             return sendData;
         }
@@ -25,7 +25,94 @@ namespace Tentcent.Ai.Sdk
             string sign = sendData.MakeSign(dic["key"]);
             sendData.SetValue("sign", sign);
             string postData = string.Join("&", sendData.GetValues().Select(x => x.Key.Trim() + "=" + sendData.UrlEncode(x.Value.ToString())).ToArray());
-            string json= BaseData.HttpPost(OcrUrl.idCardUrl, postData, Encoding.UTF8);
+            string json = BaseData.HttpPost(OcrUrl.idCardUrl, postData, Encoding.UTF8);
+            return new BaseData(json);
+        }
+
+        public static BaseData NameCard(Dictionary<string, string> dic)
+        {
+            var sendData = Init(dic);
+            sendData.SetValue("image", dic["image"]);
+            string sign = sendData.MakeSign(dic["key"]);
+            sendData.SetValue("sign", sign);
+            string postData = string.Join("&", sendData.GetValues().Select(x => x.Key.Trim() + "=" + sendData.UrlEncode(x.Value.ToString())).ToArray());
+            string json = BaseData.HttpPost(OcrUrl.nameCardUrl, postData, Encoding.UTF8);
+            return new BaseData(json);
+        }
+
+        public static BaseData Drvie(Dictionary<string, string> dic)
+        {
+            var sendData = Init(dic);
+            sendData.SetValue("image", dic["image"]);
+            //识别类型，0-行驶证识别，1-驾驶证识别
+            sendData.SetValue("card_type", dic["card_type"]);
+            string sign = sendData.MakeSign(dic["key"]);
+            sendData.SetValue("sign", sign);
+            string postData = string.Join("&", sendData.GetValues().Select(x => x.Key.Trim() + "=" + sendData.UrlEncode(x.Value.ToString())).ToArray());
+            string json = BaseData.HttpPost(OcrUrl.driveUrl, postData, Encoding.UTF8);
+            return new BaseData(json);
+        }
+
+        public static BaseData Car(Dictionary<string, string> dic)
+        {
+            var sendData = Init(dic);
+            if (dic.ContainsKey("image"))
+            {
+                sendData.SetValue("image", dic["image"]);
+            }
+            if (dic.ContainsKey("image_url"))
+            {
+                sendData.SetValue("image_url", dic["image_url"]);
+            }
+            string sign = sendData.MakeSign(dic["key"]);
+            sendData.SetValue("sign", sign);
+            string postData = string.Join("&", sendData.GetValues().Select(x => x.Key.Trim() + "=" + sendData.UrlEncode(x.Value.ToString())).ToArray());
+            string json = BaseData.HttpPost(OcrUrl.carUrl, postData, Encoding.UTF8);
+            return new BaseData(json);
+        }
+
+        public static BaseData Biz(Dictionary<string, string> dic)
+        {
+            var sendData = Init(dic);
+            sendData.SetValue("image", dic["image"]);
+            string sign = sendData.MakeSign(dic["key"]);
+            sendData.SetValue("sign", sign);
+            string postData = string.Join("&", sendData.GetValues().Select(x => x.Key.Trim() + "=" + sendData.UrlEncode(x.Value.ToString())).ToArray());
+            string json = BaseData.HttpPost(OcrUrl.bizUrl, postData, Encoding.UTF8);
+            return new BaseData(json);
+        }
+
+        public static BaseData Bank(Dictionary<string, string> dic)
+        {
+            var sendData = Init(dic);
+            sendData.SetValue("image", dic["image"]);
+            string sign = sendData.MakeSign(dic["key"]);
+            sendData.SetValue("sign", sign);
+            string postData = string.Join("&", sendData.GetValues().Select(x => x.Key.Trim() + "=" + sendData.UrlEncode(x.Value.ToString())).ToArray());
+            string json = BaseData.HttpPost(OcrUrl.bankUrl, postData, Encoding.UTF8);
+            return new BaseData(json);
+        }
+
+        public static BaseData Gen(Dictionary<string, string> dic)
+        {
+            var sendData = Init(dic);
+            sendData.SetValue("image", dic["image"]);
+            string sign = sendData.MakeSign(dic["key"]);
+            sendData.SetValue("sign", sign);
+            string postData = string.Join("&", sendData.GetValues().Select(x => x.Key.Trim() + "=" + sendData.UrlEncode(x.Value.ToString())).ToArray());
+            string json = BaseData.HttpPost(OcrUrl.genUrl, postData, Encoding.UTF8);
+            return new BaseData(json);
+        }
+
+
+        public static BaseData Hand(Dictionary<string, string> dic)
+        {
+            var sendData = Init(dic);
+            sendData.SetValue("image", dic["image"]);
+            string sign = sendData.MakeSign(dic["key"]);
+            sendData.SetValue("sign", sign);
+            string postData = string.Join("&", sendData.GetValues().Select(x => x.Key.Trim() + "=" + sendData.UrlEncode(x.Value.ToString())).ToArray());
+            string json = BaseData.HttpPost(OcrUrl.handUrl, postData, Encoding.UTF8);
             return new BaseData(json);
         }
     }
